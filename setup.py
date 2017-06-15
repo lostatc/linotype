3443a104ba744d7cbfe058090f65fdc99c3885f5
Garrett Powell <garrett@gpowell.net>
2017-06-15 09:55:16 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="linotype",
+    version="0.1",
+    description="Automatically format help messages.",
+    url="https://github.com/lostatc/linotype",
+    author="Garrett Powell",
+    author_email="garrett@gpowell.net",
+    license="GPLv3",
+    install_requires=[],
+    python_requires=">=3.5",
+    packages=["linotype"])
