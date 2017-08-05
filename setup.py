6eb25e78023327cf88a29db1efc2337ddcc019bd
Garrett Powell <garrett@gpowell.net>
2017-08-05 12:25:00 -0400

    Fix version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="linotype",
-    version="0.1",
+    version="0.1.0",
     description="Automatically format help messages.",
     url="https://github.com/lostatc/linotype",
     author="Garrett Powell",
